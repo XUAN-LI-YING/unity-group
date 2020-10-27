a1d89a8bc49686805c67853e7092569f37e30aef
XUAN-LI-YING <68537398+XUAN-LI-YING@users.noreply.github.com>
2020-10-27 15:37:03 +0800

    介面與爆炸

---
+++ Assets/animyPrefab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animyPrefab : MonoBehaviour
+{
+    public GameObject catPrefab;
+    float span = 5;
+    float delta = 0;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       this.delta+=Time.deltaTime;
+       if(this.delta>this.span){
+           this.delta=0;
+           GameObject go =Instantiate(catPrefab) as GameObject;
+           go.transform.position=new Vector3(-32,-16,0);
+       }
+       
+    }
+}
---
+++ Assets/animy_move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animy_move : MonoBehaviour
+{
+    
+    float speed=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {this.speed=0.008f;
+    transform.Translate(this.speed,0,0);
+    if(transform.position.x>32)    
+    {
+        Destroy(gameObject);
+    }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+      if(col.tag=="Trap")
+        {
+            Destroy(col.gameObject);
+             Destroy(gameObject);
+            
+        }
+    }
+}
