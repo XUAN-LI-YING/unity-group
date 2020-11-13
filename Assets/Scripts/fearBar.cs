f2360cfb4b103a03c2df156ad083df1602464ced
code-stu <adt107118@gm.ntcu.edu.tw>
2020-11-13 13:40:45 +0800

    功能-[恐懼值]-update
    Need fix : 數值圖形需要更流暢化

--- Assets/Scripts/fearBar.cs
+++ Assets/Scripts/fearBar.cs
@@ -10,19 +10,22 @@ public class fearBar : MonoBehaviour
     public int timeToAdd;
     public int timePress;
     public float delayBetweenAdd;
+    public float delayAddSec;
+
 
     // 恐懼最大最小數值
     // [Range(0,100)]
     [Header("Value")]
     public int value_Min = 0;
-    public int value_Max = 80;
+    public float value_Max = 80;
 
 
 
     //各種 UI 
     [Header("UI")]
     public Image ui_fillBar;
-    public Image ui_fillBar_effect;
+    public Image ui_Addeffect;    
+    public Image ui_Hurteffect;
     public Text ui_value;
     public Text ui_addvalue;
     public Text ui_costvalue;  
@@ -34,11 +37,12 @@ public class fearBar : MonoBehaviour
     bool waitingForInput = false;
     bool createMonster = false;
     bool combo = false;
+    bool Checking = true;
 
 
     //Other 反應事件
     [Header("FearEvent")]
-    public int points;
+    public float points;
     public int autoAddValue;
     public int addValue;   
     public int costvalue;
@@ -54,8 +58,11 @@ public class fearBar : MonoBehaviour
     
     
     }
+
+
        
     }
+
 
     //Start the game
     public void StartGame()
@@ -66,13 +73,14 @@ public class fearBar : MonoBehaviour
         ui_costvalue.text = "- " + (costvalue.ToString());
         ui_countvalue.text = timeToAdd.ToString() + "sec";
         
-        float fillAmount = points / value_Max;
-        ui_fillBar.fillAmount = fillAmount ;
+        ui_Hurteffect.fillAmount = ui_fillBar.fillAmount;
+        ui_Addeffect.fillAmount = ui_fillBar.fillAmount;
+        
         //Enable the boolean to check the input
         //打開偵測事件開關
         waitingForInput = true;
 
-       InvokeRepeating("AutoAddFear", 1, 1);  
+    //    InvokeRepeating("AutoAddFear", 1, 1);  
         //InokeRepeating 重複呼叫(“函式名”，第一次間隔幾秒呼叫，每幾秒呼叫一次)。
 
         // ui_fillBar.fillAmount = points;  
@@ -84,36 +92,86 @@ public class fearBar : MonoBehaviour
     // When pressing the Increase button
     public void Decrease()
     {
+       
         Debug.Log("正在耗費恐懼!!");
         Debug.Log($"此動作耗費了{costvalue}恐懼值");
-
         points = points - costvalue;
-        float fillAmount = points / value_Max;
-        ui_fillBar.fillAmount = fillAmount ; 
+        
+        DetectHurt();
+      
         ui_value.text = $"恐懼值下降中!!\n 目前存量={points}";
         //update points on the UI
         //更新分數顯示
-        ui_fillBar.fillAmount = points ;              
+       
+                  
     }
 
     //When pressing the Increase button
 
     public void Increase()
     {
-        points = points + timePress;
-        Debug.Log($"增加恐懼！！");
-        Debug.Log($"此動作增加了{timePress}恐懼值");
-        
-        float fillAmount = points / value_Max;
-        
-        ui_fillBar.fillAmount = fillAmount ;  
+        DetectAdd();  
+       
+        // Debug.Log($"增加恐懼！！");
+        // Debug.Log($"此動作增加了{timePress}恐懼值");
+
         //update points on the UI
         //更新分數顯示
-        ui_value.text = $"恐懼值增加{timePress}次!!\n 目前存量={points}";        
+        ui_value.text = $"恐懼值增加{timePress}!!\n 目前存量={points}";        
+        
+        Debug.Log($"{(points/value_Max)*100}% 恐懼值");
+        
+        
+        
+
             
     }   
+
+
+
+    public void DetectHurt(){
+
+
+            
+    double effectHurt = ui_Hurteffect.fillAmount;  
+        
+
+    if (effectHurt > ui_fillBar.fillAmount)
+    {
+             effectHurt -= 0.2  ;
+            Debug.Log("傷害進行中!!");
+
+    }
+ 
+    
+    }
+
+    public void DetectAdd(){
+
+    Checking = false;
+
+    points = points + timePress;
+    float fillAmount = points / value_Max;
+    ui_Addeffect.fillAmount = fillAmount;
+
+      
+            Debug.Log("add進行中!!"); 
+     
+     StartCoroutine(DelayAddEffect()); 
+    }
+
      
      
+
+
+    IEnumerator DelayAddEffect(){
+    yield return new WaitForSeconds(delayAddSec);
+        // 延遲獲得恐懼動畫
+
+         Checking = true;
+
+
+    }
     
 
     // //Checking the input for button
@@ -130,14 +188,33 @@ public class fearBar : MonoBehaviour
         {
          Increase();
         }
+        
+   
+        if (Checking)
+        {
+        //Convert the timeToClick to match the 0-1 ratio
+        // 計時器轉換成圖像表示
+        
+       
+        float fillAmount = points / value_Max;
+        //Apply the ratio to the bar UI fill amount
+        // 載入 UI 物件
+        ui_fillBar.fillAmount = fillAmount ;
+        }
+        
           
-                    
+        if (points <= value_Min  || points >= value_Max )
+        {
+          FearEvent();
+        }     
+        //判斷晉級或失敗事件  
+
     }
 
     // 自動新增恐懼值
     void AutoAddFear()
     {
-        float fillAmount = points / value_Max;
+        
         //update time on the UI
         //更新倒數顯示
         timeToAdd -= 1;
@@ -148,7 +225,7 @@ public class fearBar : MonoBehaviour
         if (timeToAdd <= 0)
         {
         CancelInvoke("AutoAddFear");
-        ui_countvalue.text =  " ☝( ◠‿◠ )☝ sec";
+        ui_countvalue.text =  " ☝ (͡° ͜ʖ ͡°) ☝ sec";
         points = points + autoAddValue;
         
         ui_value.text = points.ToString();
@@ -165,7 +242,7 @@ public class fearBar : MonoBehaviour
 
         
         // 顯示倒數幾秒增加恐懼數值 
-        Debug.Log($"{timeToAdd}");
+        // Debug.Log($"{timeToAdd}");
 
 
     
@@ -180,14 +257,33 @@ public class fearBar : MonoBehaviour
         // 重新啟動計時器
 
     }
+
+
+    IEnumerator DelayAfterAdd_1(){
+    yield return new WaitForSeconds(delayBetweenAdd);
+        // 重新設置倒數器
+        timeToAdd = 5;
+        //增加增魔所需時間  
+        ui_countvalue.text =  " LEVEL2 (͡° ͜ʖ ͡°)  ";
+        InvokeRepeating("AutoAddFear", 3, 1); 
+        // 重新啟動計時器
+
+    }
     void FearEvent(){
-        if (points == value_Min  || points < value_Min)
+        if (points <= value_Min)
         {
+           CancelInvoke("AutoAddFear");
            Debug.Log($"LOSE！");
+           ui_fillBar.fillAmount = 0 ;
+
         }
-        if (points > value_Min  || points >= value_Min)
+        if (points >= value_Min  )
         {
+           CancelInvoke("AutoAddFear");
            Debug.Log($"WIN！");
+           ui_fillBar.fillAmount = 1 ;
+        StartCoroutine(DelayAfterAdd_1()); 
+
         }
         
     }
