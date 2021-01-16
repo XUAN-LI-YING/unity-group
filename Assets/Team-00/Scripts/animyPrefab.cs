3bf24fd68b1e7e95928658256c996725b5b6e3ee
XUAN-LI-YING <68537398+XUAN-LI-YING@users.noreply.github.com>
2021-01-16 14:08:08 +0800

    merge除錯

--- Assets/Team-00/Scripts/animyPrefab.cs
+++ Assets/Team-00/Scripts/animyPrefab.cs
@@ -15,7 +15,7 @@ public class animyPrefab : MonoBehaviour
     // 怪物產生位置
     void Update()
     {
-       Debug.Log(catPrefab.transform);
+      // Debug.Log(catPrefab.transform);
        this.delta+=Time.deltaTime;
        if(this.delta>10f){
            this.delta=0;
