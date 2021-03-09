ca2302b20a41bd930ac39c7e8c03b439dbc4bd81
XUAN-LI-YING <68537398+XUAN-LI-YING@users.noreply.github.com>
2021-03-09 21:45:42 +0800

    有夠麻煩的(我方友軍)predab開啟消失button

---
+++ Assets/Team-00/Scripts/friendlyButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class friendlyButton : MonoBehaviour
+{   
+    public GameObject friendly_force;
+    public void Object_Toggle(GameObject friendly_force)
+    {   Debug.Log("我正在prefab");
+        friendly_force.SetActive( !friendly_force.activeSelf);
+    }
+}
---
+++ Assets/Team-00/Scripts/friendlyPref.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class friendlyPref : MonoBehaviour
+{
+    public GameObject friendlyForce;
+    [Header("TIME")] 
+    public float span = 1;
+    //怪物出現間隔時間
+    public float delta = 0;
+    // 怪物產生計時器
+    
+
+    // 怪物產生位置
+    void Update()
+    {
+      //如果時間累積到了5秒針，讓DELTA歸零且產生敵人
+       this.delta+=Time.deltaTime;
+       if(this.delta>5f){
+           this.delta=0;
+           GameObject go =Instantiate(friendlyForce) as GameObject;
+           //定義產生位置
+           go.transform.position=new Vector3(180,35,0);
+        }
+
+    }
+}
