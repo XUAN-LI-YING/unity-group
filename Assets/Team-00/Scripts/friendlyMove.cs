e8c383d999f780f40be23cd34d8a785eea314217
XUAN-LI-YING <68537398+XUAN-LI-YING@users.noreply.github.com>
2021-03-09 21:45:42 +0800

    有夠麻煩的(我方友軍)predab開啟消失button

---
+++ Assets/Team-00/Scripts/friendlyMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class friendlyMove : MonoBehaviour
+{
+    float speed=0;
+    void Start()
+    {
+      
+      //怪物初始數度
+      this.speed=-0.08f;  
+    }
+    void Update()
+    {
+        //怪物速度
+        transform.Translate(this.speed,0,0);
+
+        //怪物消失判斷，只靠位置
+        if(transform.position.x>200)    
+        {
+        Destroy(gameObject);
+        }
+    }
+}
