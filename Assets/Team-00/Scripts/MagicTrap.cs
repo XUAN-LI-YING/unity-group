b05f2c9ae511cdb73e18f6a932571b9bf58326c5
XUAN-LI-YING <68537398+XUAN-LI-YING@users.noreply.github.com>
2021-03-16 17:09:03 +0800

    魔法陷阱(剩攻擊扣血待完成)

---
+++ Assets/Team-00/Scripts/MagicTrap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicTrap : MonoBehaviour
+{
+
+    public GameObject aniexplo;
+
+    void OnMouseDown()
+    {
+         Instantiate(aniexplo, gameObject.transform.position, gameObject.transform.rotation);
+         Destroy(this.gameObject);
+
+      
+    }
+}
