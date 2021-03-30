1dd263a36e618cd60095c4b020856d33bb3dc521
wa.code <47618277+wastu01@users.noreply.github.com>
2021-03-30 16:59:42 +0800

    加入 yen 負責的部分

---
+++ Assets/Inventory/InventoryScripts/trapOnRoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trapOnRoad : MonoBehaviour
+{
+    public Item thisItem;
+    public Inventory playerInventory;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            AddNewItem();
+            Destroy(gameObject);
+        }          
+    }
+
+    public void AddNewItem()
+    {
+        if (!playerInventory.itemList.Contains(thisItem))
+        {
+            playerInventory.itemList.Add(thisItem);     //添加列表中項目
+            //InventoryManager.CreateNewItem(thisItem);     //因InventoryManager 使用OnEnable調用RefreshItem
+        }
+        else
+        {
+            thisItem.itemHeld += 1;     //列表持有+1
+        }
+
+        InventoryManager.RefreshItem();
+    }
+}
