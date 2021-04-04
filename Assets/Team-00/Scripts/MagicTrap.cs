30851c0ca20ce44da9bac9a66d2fe5ec3eda8e56
wa.code <adt107118@gm.ntcu.edu.tw>
2021-04-04 19:35:50 +0800

    魔法陷阱與魔力值結合
    need to fix : 沒成功觸發的狀況

--- Assets/Team-00/Scripts/MagicTrap.cs
+++ Assets/Team-00/Scripts/MagicTrap.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class MagicTrap : MonoBehaviour
 {
-
+    public static MagicTrap instance;
     public GameObject aniexplo;
 //滑鼠按下時，出現動畫aniexplo，且動畫位置為(X,y)(gameObject.transform.position, gameObject.transform.rotation)
+    
+    void Start()
+    {
+        instance = this;
+
+
+    }
+    
+    
     void OnMouseDown()
     {    
 
          Instantiate(aniexplo, gameObject.transform.position, gameObject.transform.rotation);
          Destroy(this.gameObject);
+         fearBar.instance.Increase();
 
     }
 }
