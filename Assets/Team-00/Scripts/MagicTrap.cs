c2a6be1d323b4363a5527c048f80e0fe9d89b00f
wa.code <47618277+wastu01@users.noreply.github.com>
2021-04-10 13:06:14 +0800

    修正-魔法陷阱點擊
    need to fix : 僅碰觸到陷阱的 貓咪 prefab 才能消失
    目前是全部場上貓消失
    -----------

--- Assets/Team-00/Scripts/MagicTrap.cs
+++ Assets/Team-00/Scripts/MagicTrap.cs
@@ -4,24 +4,96 @@ using UnityEngine;
 
 public class MagicTrap : MonoBehaviour
 {
-    public static MagicTrap instance;
-    public GameObject aniexplo;
-//滑鼠按下時，出現動畫aniexplo，且動畫位置為(X,y)(gameObject.transform.position, gameObject.transform.rotation)
-    
-    void Start()
+
+  public GameObject aniexplo;
+
+  // public GameObject enemy;
+
+  bool collision = false;
+
+  //滑鼠按下時，出現動畫aniexplo，且動畫位置為(X,y)(gameObject.transform.position, gameObject.transform.rotation)
+
+  void Start()
+  {
+
+    // Correct();
+
+  }
+
+  void Update()
+  {
+
+  }
+
+  public void OnTriggerExit2D(Collider2D other)
+  {
+    collision = false;
+    if (other.gameObject.tag == "Cat")
     {
-        instance = this;
+      Debug.Log("遇到貓沒點擊");
 
-
-    }
-    
-    
-    void OnMouseDown()
-    {    
-
-         Instantiate(aniexplo, gameObject.transform.position, gameObject.transform.rotation);
-         Destroy(this.gameObject);
-         fearBar.instance.Increase();
+      // Instantiate(aniexplo, gameObject.transform.position, gameObject.transform.rotation);
+      // Destroy(this.gameObject);
 
     }
+
+
+  }
+
+
+
+
+  public void OnTriggerStay2D(Collider2D other)
+  {
+    if ((other.gameObject.tag == "Cat") && (collision = true))
+    {
+
+
+      Debug.Log("meet cat");
+    }
+
+
+
+
+
+
+
+  }
+  public void OnTriggerEnter2D(Collider2D other)
+  {
+    collision = true;
+    Debug.Log("only meet ");
+  }
+
+  public void OnMouseDown()
+  {
+    if (collision == true)
+    {
+
+      Correct();
+
+    }
+    Debug.Log("click");
+    Instantiate(aniexplo, gameObject.transform.position, gameObject.transform.rotation);
+    Destroy(this.gameObject);
+
+    // 欸都 全部敵人會一起爆裂 bug !!!!!!!! 
+
+  }
+
+  void Correct()
+  {
+
+
+
+    Debug.Log("meet cat and click");
+    fearBar.instance.Increase();
+
+
+
+  }
+
+
 }
+
+
--- Assets/Team-00/Scripts/animyPrefab.cs
+++ Assets/Team-00/Scripts/animyPrefab.cs
@@ -4,36 +4,37 @@ using UnityEngine;
 
 public class animyPrefab : MonoBehaviour
 {
-    public GameObject catPrefab;
-    [Header("TIME")] 
-    public float span = 1;
-    //怪物出現間隔時間
-    public float delta = 0;
-    // 怪物產生計時器
-    
+  public GameObject catPrefab;
+  [Header("TIME")]
+  public float span = 1;
+  //怪物出現間隔時間
+  public float delta = 0;
+  // 怪物產生計時器
 
-    // 怪物產生位置
-    void Update()
+
+  // 怪物產生位置
+  void Update()
+  {
+    // Debug.Log(catPrefab.transform);
+    //如果時間累積到了5秒針，讓DELTA歸零且產生敵人
+    this.delta += Time.deltaTime;
+    if (this.delta > 10f)
     {
-      // Debug.Log(catPrefab.transform);
-      //如果時間累積到了5秒針，讓DELTA歸零且產生敵人
-       this.delta+=Time.deltaTime;
-       if(this.delta>3f){
-           this.delta=0;
-           GameObject go =Instantiate(catPrefab) as GameObject;
-           //定義產生位置
-           go.transform.position=new Vector3(-180,35,0);
-             
-    //    transform.localScale = new Vector3(1.5f, 1.5f,1.5f);
-    //更改為1.5倍大
-    //之後依據條件做為特效之類的
-           
+      this.delta = 0;
+      GameObject go = Instantiate(catPrefab) as GameObject;
+      //定義產生位置
+      go.transform.position = new Vector3(-180, 35, 0);
 
-       }
-     
-       
+      //    transform.localScale = new Vector3(1.5f, 1.5f,1.5f);
+      //更改為1.5倍大
+      //之後依據條件做為特效之類的
+
+
     }
 
 
-    //https://dometi.com.tw/blog/unity-2d-lesson3/
+  }
+
+
+  //https://dometi.com.tw/blog/unity-2d-lesson3/
 }
