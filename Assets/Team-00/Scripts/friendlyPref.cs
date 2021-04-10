565094cc89e60afd4bbcdbfde3e241ca1e0284d3
wa.code <47618277+wastu01@users.noreply.github.com>
2021-04-10 13:07:37 +0800

    [魔力值]-開啟我方友軍
    我方友軍目前只會扣一次魔力值 (因為按鈕只能開一次)

--- Assets/Team-00/Scripts/friendlyPref.cs
+++ Assets/Team-00/Scripts/friendlyPref.cs
@@ -3,31 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class friendlyPref : MonoBehaviour
-{   
-    //一開始沒有怪物為false
-    public bool activeSelf = false ;
-    public GameObject friendlyForce;
+{
+  public static friendlyPref instance;
+  //一開始沒有怪物為false
+  public bool activeSelf = false;
+  public GameObject friendlyForce;
 
-    [Header("TIME")] 
-    public float span = 1;
-    //怪物出現間隔時間
-    public float delta = 0;
-    // 怪物產生計時器
-    
+  [Header("TIME")]
+  public float span = 1;
+  //怪物出現間隔時間
+  public float delta = 0;
+  // 怪物產生計時器
 
-    // 怪物產生位置
-    void Update()
-    {  //  Debug.Log(activeSelf);
-      //如果時間累積到了5秒針，讓DELTA歸零且產生敵人
-       this.delta+=Time.deltaTime;
-       if(this.delta>2f ){ //& appear_toggle==true
-    
-           this.delta=0;
-           GameObject go =Instantiate(friendlyForce) as GameObject;
-           //定義產生位置
-           go.transform.position=new Vector3(180,33,0);
-        }
 
+  // 怪物產生位置
+  void Update()
+  {
+
+    //如果時間累積到了5秒針，讓DELTA歸零且產生敵人
+    this.delta += Time.deltaTime;
+    if (this.delta > 2f)
+    { //& appear_toggle==true
+
+      this.delta = 0;
+      GameObject go = Instantiate(friendlyForce) as GameObject;
+      //定義產生位置
+      go.transform.position = new Vector3(180, 33, 0);
     }
+
+  }
+  void Start()
+  {
+    instance = this;
+    fearBar.instance.Decrease01();
+    //目前只扣一次
+
+  }
+
+  //映璇做的部分 目前一旦按了會一直出兵哦哦
+
 
 }
