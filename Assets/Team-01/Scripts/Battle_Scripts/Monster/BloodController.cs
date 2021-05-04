8acb0e23db5907e1e63deec9556a427b9457ad0c
wa.code <47618277+wastu01@users.noreply.github.com>
2021-05-05 03:55:44 +0800

    歸類monster資料夾＋撰寫血量控制器/隨機生成物件功能

---
+++ Assets/Team-01/Scripts/Battle_Scripts/Monster/BloodController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloodController : MonoBehaviour
+{
+  public static BloodController instance;
+
+  public int hp;
+  public float max_hp;
+  public Image ui_fillBar;
+
+  public GameObject MONSTER;
+
+  // Start is called before the first frame update
+  public void Start()
+  {
+    // float ratio = ((float)hp / (float)max_hp);
+    instance = this;
+
+    Debug.Log($"血量狀態：{hp}/{max_hp}");
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float fillAmount = hp / max_hp;
+
+    ui_fillBar.fillAmount = fillAmount;
+
+
+
+    if (hp <= 0)
+    {
+      Destroy(MONSTER);
+    }
+
+  }
+  public void Costblood()
+  {
+    float fillAmount = hp / max_hp;
+    hp -= 30;
+    ui_fillBar.fillAmount = fillAmount;
+    Debug.Log($"血量狀態：{hp}/{max_hp}");
+
+  }
+}
---
+++ Assets/Team-01/Scripts/Battle_Scripts/Monster/MonsterCreater.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCreater : MonoBehaviour
+{
+
+  public GameObject Monster;
+  // public GameObject HP_Bar;
+
+
+
+  void Start()
+  {
+    //執行生成怪物程式碼(每10秒一次)
+    CreatMonster();
+    InvokeRepeating("CreatMonster", 1, 10);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+  public void CreatMonster()
+
+  {
+
+    int MonsterNum;
+
+    //隨機決定要生成幾個怪物(0-2個隨機)
+
+    MonsterNum = Random.Range(0, 3);
+
+    //開始生成怪物
+
+    for (int i = 0; i < MonsterNum; i++)
+
+    {
+
+      //宣告生成的X座標
+
+      float x;
+
+      //產生隨機的X座標
+
+      x = Random.Range(10, 132);
+
+      //生成怪物
+
+      Instantiate(Monster, new Vector3(x, 0.1f, 0), Quaternion.identity);
+
+    }
+
+  }
+
+
+}
