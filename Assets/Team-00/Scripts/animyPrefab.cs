d9b3ca607732b868dfebd79d35fe413094ad7e67
XUAN-LI-YING <68537398+XUAN-LI-YING@users.noreply.github.com>
2021-05-11 02:34:51 +0800

    壞壞unity部會算數bad 把複製到的檔案刪除、這個玩法擊退修正還是友有問題

--- Assets/Team-00/Scripts/animyPrefab.cs
+++ Assets/Team-00/Scripts/animyPrefab.cs
@@ -6,24 +6,21 @@ public class animyPrefab : MonoBehaviour
 {
   public GameObject catPrefab;
   [Header("TIME")]
-  public float span = 1;
-  //怪物出現間隔時間
+  
   public float delta = 0;
-  // 怪物產生計時器
-
-
+  
   // 怪物產生位置
   void Update()
   {
     // Debug.Log(catPrefab.transform);
     //如果時間累積到了5秒針，讓DELTA歸零且產生敵人
     this.delta += Time.deltaTime;
-    if (this.delta > 8f)
+    if (this.delta > 10f)
     {
       this.delta = 0;
       GameObject go = Instantiate(catPrefab) as GameObject;
       //定義產生位置
-      go.transform.position = new Vector3(-200, 35, 0);
+      go.transform.position = new Vector3(-200, 36, 0);
 
       //    transform.localScale = new Vector3(1.5f, 1.5f,1.5f);
       //更改為1.5倍大
