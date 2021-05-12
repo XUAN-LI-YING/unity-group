3a7fd84020873f86dff30c05ba647ac707d8972e
XUAN-LI-YING <68537398+XUAN-LI-YING@users.noreply.github.com>
2021-05-12 15:41:05 +0800

    git超級好用!! 把開始畫面UI上傳

---
+++ Assets/Team-00/Scripts/QTE.cs
@@ -0,0 +1,270 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QTE : MonoBehaviour 
+{
+    public static QTE instance;
+
+    //Time 設置判斷時間參數
+    [Header("Time")]
+    public float maxTimeToClick;
+    float timeToClick;
+    public float delayBetweenClick;
+    public int playtime;
+    [Range(0,100)]
+    public float timeRangeToClick_Min, timeRangeToClick_Max;
+
+    //UI 介面顯示
+    [Header("UI")]
+    public Image ui_fillBar;
+    public Image ui_fillBar_short;
+    public Text ui_key;
+
+    //Key handling 產生隨機字母    
+    int keyID;
+    char[] keys= "Z".ToCharArray();    
+    bool waitingForInput = false;
+    // bool waitingReturnBack = false;
+
+
+    //Other 得分數
+    [Header("Others")]
+    public int points;    
+
+    void Update()
+    {        
+        if (waitingForInput){
+        CheckingForInput();
+
+        }
+    
+
+     
+    }
+    void Start()
+    {
+        instance = this;
+
+
+    }
+ 
+
+    //Start the game
+    public void StartGame()
+    {
+        playtime = 0;
+        //Reset the click timer
+        // 重置計時器
+        timeToClick = 0;        
+        //Assign a random ID for the key
+        //隨機產生字母
+        keyID = Random.Range(0, keys.Length);        
+        //Write the key on the UI (according to the chosen keyID)
+        //顯示隨機產生字母
+        ui_key.text = keys[keyID].ToString();
+        //Reset the points
+        //重置分數
+        points = 0;
+
+        //Enable the boolean to check the input
+        // 開始偵測鍵盤輸入
+        waitingForInput = true;
+    }
+        //Checking the input for key
+    void CheckingForInput()
+    {
+        //Increase the time click key
+        // 計時器計時
+        timeToClick += Time.deltaTime;
+        //Convert the timeToClick to match the 0-1 ratio
+        // 計時器轉換成圖像表示
+        float fillAmount = timeToClick / maxTimeToClick;
+        //Apply the ratio to the bar UI fill amount
+        // 載入 UI 物件
+        ui_fillBar.fillAmount = fillAmount;
+        float v = (float)(fillAmount - 0.03);
+        // 新增導引線遮罩
+        ui_fillBar_short.fillAmount = v;
+        //    If time to click passes the total time to Click Fail
+        // 超過時間輸入 = 導引線至終點
+        if (timeToClick > (maxTimeToClick +1) )
+            {    
+            waitingForInput = false;
+            // timeToClick = 0;
+            // waitingReturnBack = true;
+            //Debug.Log($"waiting next");
+            playtime += 1;
+            ui_key.text = $"Miss\n失敗第{playtime}次";
+            StartCoroutine(DelayAfterFailed());   
+            }
+        //Check all keyboard input
+        // 確認所有按鍵輸入
+        foreach (KeyCode kcode in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            // kcode.ToString().Length==1 &&
+
+            if ( Input.GetKeyDown(kcode) )
+            {
+                if (char.Parse(kcode.ToString()) == keys[keyID] && IsInRange() )
+                    {
+                    //Debug.Log($"Correct check");
+                    
+                    Correct();
+                    }
+                else
+                {
+                                   
+                //If it's not the same call Failed method
+                  
+                    //Debug.Log($"failed key");
+                    
+                    Failed(); 
+                }    
+                
+ 
+           
+            }
+
+
+
+
+                     
+        }
+         if (playtime > 2)
+        {
+            waitingForInput = false;
+            timeToClick = 0;
+
+            ui_key.text = $"還敢失敗rr\n失敗第{playtime}次";
+            Debug.Log("END QTE");
+            // QTE 無效扣恐懼值
+
+        }
+    }
+
+    //Check if in range of time input
+    // 確認是否在正確範圍輸入
+    bool IsInRange()
+    {
+        //Getting the 0-1 value then x100 to make equivalent to 100%
+        
+        float currentRatio = (timeToClick / maxTimeToClick)*100;
+        //Check if the current time ratio is between the Min and the Max
+        if (currentRatio > timeRangeToClick_Min && currentRatio < timeRangeToClick_Max)
+        {
+          //Debug.Log(currentRatio);            
+          //Debug.Log(timeRangeToClick_Min);
+          //Debug.Log(timeRangeToClick_Max);
+         
+
+            //Debug.Log($"IsInRange-true");
+            return true;
+            
+            
+        }
+
+            
+        else
+        //Debug.Log(currentRatio);            
+        //Debug.Log($"IsInRange-false");
+           return false;
+           
+    }
+
+    //When pressing the correct key
+    void Correct()
+    {
+        waitingForInput = false;
+        // 顯示成功並落在哪個區間
+        //Debug.Log($"Correct Key");
+        //Debug.Log($"Clicked at ratio {(timeToClick / maxTimeToClick) * 100}");
+        //Disable the boolean for input check
+        //停止偵測鍵盤輸入
+        
+        //Gain point
+        // 得分數+1
+        points++;
+        //Write points on the UI
+        // 顯示成功共得幾分
+        ui_key.text = $"獲得 {points} 分";        
+        //Reset the bar
+        // 重置導引線
+        ui_fillBar.fillAmount = 0;
+        //增加恐懼值
+        // call fearbar script
+        
+        fearBar.instance.Increase();
+
+
+        
+        //Call the delay before next key
+        // 開始等待下次判斷時間
+        StartCoroutine(DelayAfterCorrect());           
+    }   
+
+    IEnumerator DelayAfterCorrect()
+    {
+        //Wait (delayBetweenClick) seconds
+        // 輸入等待時間
+        yield return new WaitForSeconds(delayBetweenClick);
+        //Reset the click timer
+        // 重置計時器
+        timeToClick = 0;
+        //Assign a random ID for the key
+        keyID = Random.Range(0, keys.Length);
+        //Write the key on the UI (according to the chosen keyID)
+        ui_key.text = keys[keyID].ToString();
+
+        //Enable the boolean to check the input
+        waitingForInput = true;
+        
+    }
+    IEnumerator DelayAfterFailed()
+    {
+        yield return new WaitForSeconds(delayBetweenClick);
+ 
+
+         //Reset the click timer
+        // 重置計時器
+        timeToClick = 0;
+        //Assign a random ID for the key
+        keyID = Random.Range(0, keys.Length);
+        //Write the key on the UI (according to the chosen keyID)
+        ui_key.text = keys[keyID].ToString();
+
+        //Enable the boolean to check the input
+        waitingForInput = true;
+
+    }
+   
+      //When clicking the wrong key
+    void Failed()
+    {
+         waitingForInput = false;
+         fearBar.instance.Decrease();
+
+
+        // 顯示失敗並落在哪個區間
+        Debug.Log("lose");
+        Debug.Log($"Clicked at ratio {(timeToClick / maxTimeToClick) * 100}");
+        //Stop the boolean for input check
+        //停止偵測鍵盤輸入
+       
+        //Write losing text on UI
+        // 顯示失敗 
+        playtime += 1;
+        ui_key.text = $"Miss\n失敗第{playtime}次";
+        //Reset the bar
+        // 重置導引線
+        ui_fillBar.fillAmount = 0; 
+  
+        StartCoroutine(DelayAfterFailed());                   
+    }
+
+
+
+
+
+}
---
+++ Assets/Team-00/Scripts/fearBar.cs
@@ -0,0 +1,365 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class fearBar : MonoBehaviour
+{
+  public static fearBar instance;
+  // 時間參數
+  [Header("Timer")]
+  public int timeToAdd;
+  //倒數幾秒
+  public int timePress;
+  //QTE連擊次數 (先留者特殊加分用?)
+  public float delayBetweenAdd;
+  //倒數器間隔時間
+  public float delaySec;
+  // 扣血動畫延遲時間
+
+
+
+  // 恐懼最大最小數值
+  // [Range(0,100)]
+  [Header("Value")]
+  public int value_Min = 0;
+  public float value_Max = 200;
+
+
+  //各種 UI 
+  [Header("UI")]
+  public Image ui_fillBar;
+  //主要血條 淺綠色
+  public Image ui_Addeffect;
+  public Image ui_Hurteffect;
+  public Text ui_value;
+  public Text ui_addvalue;
+  public Text ui_costvalue;
+  public Text ui_countvalue;
+
+
+  bool waitingForInput = false;
+  bool createMonster = false;
+  bool combo = false;
+  bool Checking = true;
+
+
+  //Other 反應事件
+  [Header("FearEvent")]
+  public float points;
+  //目前存量值
+  public int autoAddValue;
+  //倒數增加”值“
+  public int addValue;
+  //一般情況增加值
+  public int costvalue;
+  //一般情況耗費值
+  public int eachpresscost;
+  //出動友軍耗費值(單次)
+  //關聯-- decrease01 函式
+
+  // 持續偵測有無事件
+  void Update()
+  {
+
+    if (waitingForInput)
+    {
+
+      CheckingForInput();
+
+
+    }
+
+
+
+  }
+  void Start()
+  {
+    instance = this;
+    StartGame();
+
+  }
+  void CheckingForInput()
+  {
+    //創立小怪事件
+    if (createMonster)
+    {
+
+
+      Decrease();
+    }
+    //連段QTE
+    if (combo)
+    {
+      Increase();
+    }
+
+
+    if (Checking)
+    {
+      //Convert the timeToClick to match the 0-1 ratio
+      // 計時器轉換成圖像表示
+      // 特效動畫完成後真正數值圖形增加
+
+      float fillAmount = points / value_Max;
+      //Apply the ratio to the bar UI fill amount
+      // 載入 UI 物件
+      ui_fillBar.fillAmount = fillAmount;
+    }
+
+
+    if (points < value_Min || points >= value_Max)
+    {
+      waitingForInput = false;
+      CancelInvoke("AutoAddFear");
+      FearEvent();
+     
+    }
+    //判斷晉級或失敗事件  
+
+  }
+
+
+  //Start the game
+  public void StartGame()
+  {
+    waitingForInput = true;
+    // float fillAmount = points / value_Max;
+    // points = 0 顯示初始數值
+    ui_value.text = points.ToString() + " FearPoint";
+    // ui_addvalue.text = (autoAddValue.ToString()) + "points/sec";
+    // ui_costvalue.text = "- " + (costvalue.ToString());
+    ui_countvalue.text = timeToAdd.ToString() + "sec";
+
+    ui_Hurteffect.fillAmount = ui_fillBar.fillAmount;
+    ui_Addeffect.fillAmount = ui_fillBar.fillAmount;
+
+    //Enable the boolean to check the input
+    //打開偵測事件開關
+    waitingForInput = true;
+
+    InvokeRepeating("AutoAddFear", 1, 1);
+    //InokeRepeating 重複呼叫(“函式名”，第一次間隔幾秒呼叫，每幾秒呼叫一次)。
+  }
+
+
+  // 顯示耗費多少恐懼
+  // When pressing the Increase button
+  public void Decrease()
+  {
+
+    Debug.Log($"此動作耗費了{costvalue}魔力值");
+    DetectHurt();
+
+    ui_value.text = $"魔力值下降中!!\n 目前存量={points}";
+    //update points on the UI
+    //更新分數顯示
+
+
+  }
+  public void Decrease01()
+  {
+    Checking = false;
+    points = points - eachpresscost;
+    float fillAmount = points / value_Max;
+    ui_value.text = $"魔力值損耗{eachpresscost}!!\n 目前存量={points}";
+    Debug.Log($"出動友軍耗費{eachpresscost}魔力值");
+    ui_fillBar.fillAmount = fillAmount;
+
+
+    StartCoroutine(DelayCostEffect());
+    StartCoroutine(RepeatDecrease());
+  }
+
+  //When pressing the Increase button
+
+  public void Increase()
+  {
+    DetectAdd();
+
+    // Debug.Log($"增加恐懼！！");
+    // Debug.Log($"此動作增加了{timePress}魔力值");
+
+    //update points on the UI
+    //更新分數顯示
+
+    if (combo)
+    {
+      ui_value.text = $"魔力值增加 {timePress} !!\n 目前存量={points}";
+    }
+    // 本來用在 QTE 後來取消先留著 特殊加分
+
+    // 其他一般狀況
+    else
+    {
+
+      ui_value.text = $"魔力值增加{timePress}!!\n 目前存量={points}";
+
+      Debug.Log($"{(points / value_Max) * 100}% 魔力值");
+    }
+
+
+
+
+
+
+  }
+
+
+
+  public void DetectHurt()
+  {
+
+    Checking = false;
+
+    points = points - costvalue;
+    float fillAmount = points / value_Max;
+    ui_fillBar.fillAmount = fillAmount;
+
+    Debug.Log("傷害進行中!!");
+
+
+    StartCoroutine(DelayCostEffect());
+    StartCoroutine(RepeatDecrease());
+  }
+
+  public void DetectAdd()
+  {
+
+    Checking = false;
+
+    points = points + timePress;
+
+    float fillAmount = points / value_Max;
+
+    ui_Addeffect.fillAmount = fillAmount;
+
+
+    Debug.Log("add進行中!!");
+
+    StartCoroutine(DelayAddEffect());
+    StartCoroutine(RepeatDecrease());
+  }
+
+
+
+
+
+
+  IEnumerator DelayAddEffect()
+  {
+    yield return new WaitForSeconds(delaySec);
+    // 延遲獲得恐懼動畫
+    float fillAmount = points / value_Max;
+    ui_fillBar.fillAmount = fillAmount;
+    waitingForInput = true;
+
+  }
+  //這是特效動畫的延遲！！！ 我看錯好幾次了註解一下
+
+  IEnumerator DelayCostEffect()
+  {
+    yield return new WaitForSeconds(delaySec);
+    // 延遲獲得恐懼動畫
+    float fillAmount = points / value_Max;
+    ui_Hurteffect.fillAmount = fillAmount;
+
+  }
+
+
+  IEnumerator RepeatDecrease()
+  {
+    yield return new WaitForSeconds(delaySec);
+    Checking = true;
+
+  }
+
+  // //Checking the input for button
+
+
+  // 自動新增魔力值
+  void AutoAddFear()
+  {
+
+    //update time on the UI
+    //更新倒數顯示
+    timeToAdd -= 1;
+    ui_countvalue.text = timeToAdd.ToString() + "sec";
+
+    //小於零停止倒數計時
+    //並秀出 ☝( ◠‿◠ )☝ 文字
+    if (timeToAdd <= 0)
+    {
+      waitingForInput = false;
+      CancelInvoke("AutoAddFear");
+      ui_countvalue.text = "O(∩_∩)O";
+      points = points + autoAddValue;
+      float fillAmount = points / value_Max;
+      ui_Addeffect.fillAmount = fillAmount;
+      ui_value.text = points.ToString();
+
+      StartCoroutine(DelayAddEffect());
+      // 兩個delay搞錯.... 這是特效(effect)的delay 
+
+      // 延遲幾秒重新計時 time delay 
+      StartCoroutine(DelayAfterAdd());
+    }
+
+
+    // 顯示倒數幾秒增加恐懼數值 
+    // Debug.Log($"{timeToAdd}");
+
+
+
+  }
+
+
+  IEnumerator DelayAfterAdd()
+  {
+    yield return new WaitForSeconds(delayBetweenAdd);
+    // 重新設置倒數器
+    timeToAdd = 4;
+    InvokeRepeating("AutoAddFear", 1, 1);
+    // 重新啟動計時器
+
+  }
+
+
+  IEnumerator RunSecvalue()
+  {
+    yield return new WaitForSeconds(delayBetweenAdd);
+    ui_fillBar.fillAmount = 0;
+    // 重新設置倒數器
+    timeToAdd = 5;
+    //增加增魔所需時間  
+    ui_countvalue.text = " LEVEL2 (͡° ͜ʖ ͡°)  ";
+    InvokeRepeating("AutoAddFear", 3, 1);
+    // 重新啟動計時器
+
+  }
+  void FearEvent()
+  {
+    if (points < value_Min)
+    {
+     
+      Debug.Log($"LOSE！");
+      ui_fillBar.fillAmount = 0;
+
+    }
+    if (points >= value_Max)
+    {
+      
+      //   Debug.Log($"WIN！");
+      // ui_fillBar.fillAmount = 1;
+      StartCoroutine(RunSecvalue());
+
+
+    }
+
+  }
+
+
+
+
+}
