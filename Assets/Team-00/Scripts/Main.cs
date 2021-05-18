8562da6ea4e26454102300ff292902a7c543d273
patapatapan <johnny8947276@gmail.com>
2021-05-18 22:17:22 +0800

    套用陷阱觸發、敵人destroy未解決、暗部移動有問題

--- Assets/Team-00/Scripts/Main.cs
+++ Assets/Team-00/Scripts/Main.cs
@@ -8,6 +8,8 @@ public class Main : MonoBehaviour
     public Player Player;
     public Collider2D Ladder;
     // Use this for initialization
+    public Camera cam;
+    public LayerMask groundLayer;
     void Start()
     {
 
@@ -17,6 +19,7 @@ public class Main : MonoBehaviour
     void Update()
     {
         InputUpdate();
+       
     }
 
     void InputUpdate()
@@ -25,15 +28,18 @@ public class Main : MonoBehaviour
         Vector3 mousePoint = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
         if (Physics2D.Raycast(mousePoint, Vector2.zero)) return;
 
-        RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.down, 1);
+        RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.down, groundLayer);
         if (hit == false) return;
 
         Vector3 pos = hit.point;
-        pos = new Vector3(pos.x, pos.y+0.5f, pos.z);
-        if (Player.IsLadder) Player.MoveTo(pos, "Player@Back");
-        if (Mathf.Abs(pos.y - Player.transform.position.y) <= 1) Player.MoveTo(pos, "Player@Walk");
+        pos = new Vector3(pos.x, pos.y+5, pos.z);
         
+        if (Player.IsLadder) Player.MoveTo(pos, "Player@Back");
+        if (Mathf.Abs(pos.y - Player.transform.position.y) >= 1) Player.MoveTo(pos, "Player@Walk");
+        Debug.Log(Mathf.Abs(pos.y - Player.transform.position.y));
         MoveCursor.transform.position = pos;
         MoveCursor.SetActive(true);
+        Debug.Log(pos);
+        Debug.Log(Player.CurrentPos.y);
     }
 }
