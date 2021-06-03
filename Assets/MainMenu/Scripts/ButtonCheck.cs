60fc8ab925a65618b9940bf5f1adffc70efc6e2d
mrwang01 <47618277+wastu01@users.noreply.github.com>
2021-06-03 13:26:08 +0800

    新增場景-[MainMenu]
    need to fix : 開始畫面要切換至 MainMenu

---
+++ Assets/MainMenu/Scripts/ButtonCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCheck : MonoBehaviour
+{
+    // public static ButtonCheck instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // instance = this;
+        
+        //切換頁面
+        // onmouseclick > UI回饋 change color or hover > change scene
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if onmouseclick is false {
+        
+        // 原本按鈕狀態
+            
+        // }
+    }
+    public void changeScene(){
+
+         Debug.Log($"此動作切換至A頁面");
+
+    }
+}
