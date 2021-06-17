97cf06d82050f8da1bdfe2f149684aa9ec92b98c
mrwang01 <47618277+wastu01@users.noreply.github.com>
2021-06-17 19:43:27 +0800

    整理script資料夾

---
+++ Assets/Team-00/Scripts/RandomNumber/DataManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static DataManager instance;
+
+
+
+    void Start(){
+        instance = this;
+
+
+    }
+}
