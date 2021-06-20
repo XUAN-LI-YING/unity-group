bbf4d4e244568aa6fa76dfcdefd5d4d42ef34046
mrwang01 <47618277+wastu01@users.noreply.github.com>
2021-06-20 22:50:44 +0800

    加入陷阱判斷碰撞 Script

---
+++ Assets/Team-00/Scripts/TrapObject/DarkTrap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkTrap : MonoBehaviour
+{
+    public GameObject enemy;
+    bool collision = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        collision = true;
+        // Debug.Log("only catch sth");
+
+            if (other.gameObject.tag == "Cat")
+            {
+            Debug.Log("遇到守衛");
+
+            }
+    }
+    void Correct()
+    {
+        Debug.Log("catch enemy");
+        fearBar.instance.Increase();
+
+
+
+    }
+}
