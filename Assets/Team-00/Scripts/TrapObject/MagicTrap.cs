2fe76282835c14835906a95d9aabe65a84691600
wa.code <47618277+wastu01@users.noreply.github.com>
2021-11-27 23:43:23 +0800

    優化程式-[魔法地雷]
    詳細變更請看 git diff

--- Assets/Team-00/Scripts/TrapObject/MagicTrap.cs
+++ Assets/Team-00/Scripts/TrapObject/MagicTrap.cs
@@ -9,7 +9,7 @@ public class MagicTrap : MonoBehaviour
 
    
 
-   bool collision = false;
+   public bool collision = false;
 
   //滑鼠按下時，出現動畫Aniboom，且動畫位置為(X,y)(gameObject.transform.position, gameObject.transform.rotation)
 
@@ -17,15 +17,20 @@ public class MagicTrap : MonoBehaviour
 
    public void OnTriggerEnter2D(Collider2D other)
    {
-     collision = true;
+    //  collision = true;
     //  Debug.Log("only meet sth");
+   }   
+   public void OnTriggerStay2D(Collider2D other)
+   {
+     collision = true;
+    
    }
+
    public void OnTriggerExit2D(Collider2D other)
   {
      collision = false;
      if (other.gameObject.tag == "Cat")
     {
-      // Debug.Log("遇到敵方沒點擊");
 
     }
 
@@ -35,27 +40,15 @@ public class MagicTrap : MonoBehaviour
 
   void update()
   {
-    // if ( Input.GetMouseButtonDown(0) )
-    // {
-    //    Instantiate(Aniboom, gameObject.transform.position, gameObject.transform.rotation);
-    //    Destroy(this.gameObject);
+
     if(Input.GetMouseButtonDown(0))
     {
       	Debug.Log("Pressed left click.");
     }
 		
 
-       
-    if (collision == true)
-     {
-
-      Correct();
-     }
-     
-     
   }
 
-  //這串function無法觸發
   void OnMouseDown()
   {
      Debug.Log("滑鼠在點我了!");
@@ -64,16 +57,17 @@ public class MagicTrap : MonoBehaviour
 
       Correct();
      }
+
+       Instantiate(Aniboom, gameObject.transform.position, gameObject.transform.rotation);
+       Destroy(this.gameObject);
     
-    
-    // 日後再自動產生
 
   }
 
   void Correct()
   {
     
-    //  Debug.Log("click correct");
+     Debug.Log("click correct");
     fearBar.instance.Increase();
 
 
