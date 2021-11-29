5f0e3032f6aff133ca27b5b730cf8d9d66be4ec5
patapatapan <johnny8947276@gmail.com>
2021-11-29 18:17:34 +0800

    Merge branch '01' of https://github.com/XUAN-LI-YING/unity-group into 01
    # Conflicts:
    #	Assembly-CSharp.csproj
    #	Assets/Team-00/Scenes/DevilDefend.unity
    #	Assets/Team-00/Scripts/Enemy/animy_move.cs
    #	ProjectSettings/ProjectVersion.txt
    新增5個slot
    *個別陷阱

---
+++ Assets/Team-00/Scripts/Slot/Slot4.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//我也不知道叫甚麼
+public class Slot4 : MonoBehaviour
+{
+    public Item slotItem;
+    public Image slotImage;
+    //public Text slotNum;
+    //public string slotInfo;   //物件資訊 字串
+
+    public GameObject itemInSlot;
+    /*public void  ItemOnClicked()  //按鈕點擊
+    {
+        
+        InventoryManager.UpdateItemInfo(slotItem.itemInfo);   //更新物件資訊
+    }*/
+    
+    public void SetupSlot(Item item)
+    {
+        if (item == null)    //若值為空
+        {
+            itemInSlot.SetActive(false);    //不顯示
+            return;
+        }
+        slotItem = item;
+        slotImage.sprite = item.itemImage;
+        //slotNum.text = item.itemHeld.ToString();
+        //slotInfo = item.itemInfo;
+    }
+}
