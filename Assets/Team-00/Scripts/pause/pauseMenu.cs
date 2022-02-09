2403f375c130336d4362ed64cf3f714377514450
XUAN-LI-YING <68537398+XUAN-LI-YING@users.noreply.github.com>
2022-02-09 18:07:22 +0800

    暫停畫面與暫停菜單完成<3

---
+++ Assets/Team-00/Scripts/pause/pauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+public class pauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true); //顯示暫停畫面
+        Time.timeScale=0f;         //遊戲畫面暫停
+    }
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false); //不顯示暫停畫面
+         Time.timeScale=1f;         //遊戲畫面不暫停
+    }
+
+     public void QuitGame()
+   {
+       Application.Quit();
+      EditorApplication.isPlaying = false;
+   }
+
+    public void LoadScene(string sceneName)
+    {
+       SceneManager.LoadScene(sceneName);
+        Time.timeScale=1f;
+    }
+}
