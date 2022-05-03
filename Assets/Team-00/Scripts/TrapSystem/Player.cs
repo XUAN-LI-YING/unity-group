458b4538ce6aaa7e524625d8e73030cd7733f63c
wa.code <47618277+wastu01@users.noreply.github.com>
2022-05-03 20:11:22 +0800

    陷阱扣魔

--- Assets/Team-00/Scripts/TrapSystem/Player.cs
+++ Assets/Team-00/Scripts/TrapSystem/Player.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player instance;
     public Main Main;
     //[SerializeField] private float speed = 0.1f;
     Animator animator;
     public SpriteRenderer Sprite;
-    public  bool IsLadder;
+    public bool IsLadder;
     //public bool Istbox;
     //public bool Isbbox; 
     public Collider2D Collider2D;
@@ -17,10 +18,12 @@ public class Player : MonoBehaviour
     public Vector3 GoalPos;
     public Vector2 CurrentPos;
 
-    public int cdtime = 0;
+    public bool isLaunch;
 
     private void Start()
     {
+        instance = this;
+        isLaunch = false;
         GoalPos = transform.position;       //定位
         this.animator = GetComponent<Animator>();
     }
@@ -34,13 +37,13 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Move();
-        
+
         //Movement();
         //Debug.Log(CurrentPos.x);
         //Debug.Log(transform.position.y);
         //Debug.Log()
     }
-    
+
     /*void LateUpdate()
     {
         if(transform.position.y > 16)
@@ -50,49 +53,49 @@ public class Player : MonoBehaviour
     }*/
     private void Move()
     {
-        if(!Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+        if (!Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
         {
-            if(gameObject.transform.position.x <= 85.5 && gameObject.transform.position.x >= -90.5)
-            if(Input.GetKey(KeyCode.A) && gameObject.transform.position.x > -89)
+            if (gameObject.transform.position.x <= 85.5 && gameObject.transform.position.x >= -90.5)
+                if (Input.GetKey(KeyCode.A) && gameObject.transform.position.x > -89)
+                {
+                    gameObject.transform.position += new Vector3(-0.9f, 0, 0);
+                    animator.Play("Player@Walk");
+                    Sprite.flipX = false;
+                }
+            if (Input.GetKey(KeyCode.D) && gameObject.transform.position.x < 85)
             {
-                gameObject.transform.position += new Vector3(-0.9f,0,0);
-                animator.Play("Player@Walk");
-                Sprite.flipX = false;
-            }
-            if(Input.GetKey(KeyCode.D) && gameObject.transform.position.x < 85)
-            {
-                gameObject.transform.position += new Vector3(0.9f,0,0);
+                gameObject.transform.position += new Vector3(0.9f, 0, 0);
                 animator.Play("Player@Walk");
                 Sprite.flipX = true;
             }
-            if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
             {
                 animator.Play("Player@Idle");
             }
         }
-        
+
 
         if (IsLadder == true)
         {
-            if(gameObject.transform.position.y <= 16.2 && gameObject.transform.position.y >= -20.5)
+            if (gameObject.transform.position.y <= 16.2 && gameObject.transform.position.y >= -20.5)
             {
-            if(Input.GetKey(KeyCode.W) && gameObject.transform.position.y < 16)
-            {
-                gameObject.transform.position += new Vector3(0,0.07f,0);
-                animator.Play("Player@Climb");
-                //if(transform.position.y >= 14.1)
-                //gameObject.transform.position += new Vector3(0,0,0);
-                //Debug.Log("1");
+                if (Input.GetKey(KeyCode.W) && gameObject.transform.position.y < 16)
+                {
+                    gameObject.transform.position += new Vector3(0, 0.07f, 0);
+                    animator.Play("Player@Climb");
+                    //if(transform.position.y >= 14.1)
+                    //gameObject.transform.position += new Vector3(0,0,0);
+                    //Debug.Log("1");
+                }
+                if (Input.GetKey(KeyCode.S) && gameObject.transform.position.y > -20)
+                {
+                    gameObject.transform.position += new Vector3(0, -0.07f, 0);
+                    animator.Play("Player@Climb");
+                    //if(transform.position.y <= -18.8)
+                    //gameObject.transform.position += new Vector3(0,0,0);
+                    //Debug.Log("2");
+                }
             }
-            if(Input.GetKey(KeyCode.S) && gameObject.transform.position.y > -20)
-            {
-                gameObject.transform.position += new Vector3(0,-0.07f,0);
-                animator.Play("Player@Climb");
-                //if(transform.position.y <= -18.8)
-                //gameObject.transform.position += new Vector3(0,0,0);
-                //Debug.Log("2");
-            }
-            }            
         }
 
         /*if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
@@ -108,15 +111,14 @@ public class Player : MonoBehaviour
         Collider2D.enabled = true;
     }
 
-    public void Coldtime()
+    public void LaunchTrap()
     {
 
-        // IDK_HOW_CD_SYSTEM_WORK_AND_WHAT_DATA_I_SHOULD_USE = true;
-
-        // MAY_BE_IS_TIME -= 1;
-
+        isLaunch = true;
 
     }
+
+    
 
     public void OnTriggerEnter2D(Collider2D other)//接觸梯子物件 
     {
@@ -135,59 +137,59 @@ public class Player : MonoBehaviour
         //if (other.gameObject.CompareTag ("tbox"))
         /*if (other != Main.tbox) return;
         Istbox = true;*/
-        
+
         //if (other.gameObject.CompareTag ("bbox"))
         /*if (other != Main.bbox) return;
         Isbbox = true;*/
 
         // 碰到 觸發
 
-       
-       
+    if (isLaunch)
+    {
+        
+
         if (other.tag == "Mg_LandimineTrap")
         {
-            if (cdtime == 0)
-            {
-             fearBar.instance.MgmineBuild();
-             Debug.Log("觸發mine");
-                
-            }
 
-            
+            fearBar.instance.MgmineBuild();
+            Debug.Log("觸發mine");
+
+
         }
 
         if (other.tag == "DarkTrap")
         {
-             fearBar.instance.DarktrapBuild();
-             Debug.Log("觸發dark");
-            
-            
-        } 
+            fearBar.instance.DarktrapBuild();
+            Debug.Log("觸發dark");
+
+
+        }
         if (other.tag == "Trap-05")
         {
-             fearBar.instance.BigtrapBuild();
-             Debug.Log("觸發大規模");
-            
-            
-        }         
+            fearBar.instance.BigtrapBuild();
+            Debug.Log("觸發大規模");
+
+
+        }
         if (other.tag == "SpikedTrap")
         {
-             fearBar.instance.BigtrapBuild();
-             Debug.Log("觸發尖刺");
-            
-            
-        } 
+            fearBar.instance.BigtrapBuild();
+            Debug.Log("觸發尖刺");
+
+
+        }
         if (other.tag == "Cat")
         {
-           
-            //  Debug.Log("cattttttt");            
-            
-        } 
 
-        
-        
+            //  Debug.Log("cattttttt");            
+
+        }
+
     }
-    
+
+
+    }
+
 
     void OnTriggerExit2D(Collider2D other)//離開物件
     {
