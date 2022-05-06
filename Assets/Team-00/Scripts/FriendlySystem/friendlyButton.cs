cb1d0050ea3e9978424a37e604a97573f21e48eb
wa.code <47618277+wastu01@users.noreply.github.com>
2022-05-06 10:46:37 +0800

    友軍按鈕除錯

--- Assets/Team-00/Scripts/FriendlySystem/friendlyButton.cs
+++ Assets/Team-00/Scripts/FriendlySystem/friendlyButton.cs
@@ -10,8 +10,6 @@ public class friendlyButton : MonoBehaviour
     public static friendlyButton instance;
     // public GameObject friendly_force;
     // public bool appear_toggle = false;
-    //點擊時friendly_force這個空物(並之後掛上friendlyforce物件)會去掛上的物件friendlyforce找到裡面的activeSelf，
-    //並把他變否
 
     public GameObject turnonoff;
     public Sprite StopbuttonImage;
