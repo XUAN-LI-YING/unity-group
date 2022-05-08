1971efdd6c5ac6b938fd8c05992a7fb4133fa649
wa.code <47618277+wastu01@users.noreply.github.com>
2022-05-09 03:25:56 +0800

    場上放一隻敵方方便測試用

--- Assets/Team-00/Scripts/EnemySystem/EnemyPrefab.cs
+++ Assets/Team-00/Scripts/EnemySystem/EnemyPrefab.cs
@@ -40,7 +40,7 @@ public class EnemyPrefab : MonoBehaviour
 
     instance = this;
     delta = 0;
-    createtime = 8; 
+    createtime = 18; 
     createNumber=1;
     Instance=this;
     enemyDie=0;
